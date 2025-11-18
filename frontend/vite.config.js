e2f76da5246ece405bbcb6386552726569910925
Gaurav Goswami <142787056+itzgauravgoswami@users.noreply.github.com>
2025-11-18 23:34:04 +0530

    Error Solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})git remote add origin https://github.com/itzgauravgoswami/MedLife.git
+})
